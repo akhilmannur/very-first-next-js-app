11dc1e1fe27a76dc8188279aa3cb01053c6f0a05
Akhilanandck <akhilmannur974@gmail.com>
2024-01-30 15:43:33 +0530

    edited

---
+++ app/users/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const UsersPage = async() => {
+
+  interface User{
+    id:number,
+    name:string
+  }
+
+const res= await fetch('https://jsonplaceholder.typicode.com/users')
+const users: User[] =await res.json();
+
+  return (
+    <div>
+      <h1>
+        User List 
+      </h1>
+      <ul>
+        {
+          users.map((user)=>(
+           <li key={user.id} >
+              {user.name}
+           </li>
+          ))
+        }
+      </ul>
+   
+    </div>
+  );
+};
+
+export default UsersPage;
